dbb31fc6f4bfb76c54bcb2cbe0df9bfe814abc4c
paulooosf <phpessoal2021@gmail.com>
2024-09-06 19:53:50 -0300

    feat: finalizando controlador

--- src/main/java/io/github/paulooosf/encurtador/controller/LinkController.java
+++ src/main/java/io/github/paulooosf/encurtador/controller/LinkController.java
@@ -1,4 +1,42 @@
 package io.github.paulooosf.encurtador.controller;
 
+import io.github.paulooosf.encurtador.dto.LinkDTO;
+import io.github.paulooosf.encurtador.dto.LinkInfoDTO;
+import io.github.paulooosf.encurtador.service.LinkService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/link")
 public class LinkController {
+
+    @Autowired
+    LinkService service;
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<LinkDTO> acessar (@PathVariable String codigo) {
+        return ResponseEntity.ok(service.acessar(codigo));
+    }
+
+    @GetMapping("/info/{id}")
+    public ResponseEntity<LinkInfoDTO> buscar (@PathVariable Long id) {
+        return ResponseEntity.ok(service.buscar(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<LinkDTO> encurtar (String link) {
+        return ResponseEntity.ok(service.encurtar(link));
+    }
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<LinkDTO> editar (@PathVariable Long id, String link) {
+        return ResponseEntity.ok(service.editar(id, link));
+    }
+
+    @DeleteMapping("/remover/{id}")
+    public ResponseEntity<LinkDTO> deletar (@PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
