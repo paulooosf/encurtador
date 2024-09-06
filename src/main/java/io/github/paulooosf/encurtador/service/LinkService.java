65c6326476119e4c2063db61f9a4640680062b86
paulooosf <phpessoal2021@gmail.com>
2024-09-06 19:22:21 -0300

    feat: finalizando classe de serviço

--- src/main/java/io/github/paulooosf/encurtador/service/LinkService.java
+++ src/main/java/io/github/paulooosf/encurtador/service/LinkService.java
@@ -1,6 +1,7 @@
 package io.github.paulooosf.encurtador.service;
 
 import io.github.paulooosf.encurtador.dto.LinkDTO;
+import io.github.paulooosf.encurtador.dto.LinkInfoDTO;
 import io.github.paulooosf.encurtador.exception.NotFoundException;
 import io.github.paulooosf.encurtador.model.Link;
 import io.github.paulooosf.encurtador.repository.LinkRepository;
@@ -30,11 +31,41 @@ public class LinkService {
         return new LinkDTO(link.getLink());
     }
 
+    public LinkInfoDTO buscar (Long id) {
+        Optional<Link> linkOpt = repository.findById(id);
+        if (linkOpt.isEmpty()) {
+            throw new NotFoundException();
+        }
+        return new LinkInfoDTO(linkOpt.get());
+    }
+
     public LinkDTO encurtar (String linkOriginal) {
         Link link = new Link(linkOriginal, GeradorCodigos.gerarCodigoAleatorio(), LocalDateTime.now(), 0);
         repository.save(link);
 
-        URI uri = Servle
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .buildAndExpand(link.getCodigo())
+                .toUri();
+        return new LinkDTO(uri.toString());
+    }
+
+    public LinkDTO editar (Long id, String linkEditado) {
+        Optional<Link> linkOpt = repository.findById(id);
+        if (linkOpt.isEmpty()) {
+            throw new NotFoundException();
+        }
+        Link link = linkOpt.get();
+        link.setLink(linkEditado);
+        repository.save(link);
+
         return new LinkDTO(link.getLink());
+    }
+
+    public void deletar(Long id) {
+        if (!repository.existsById(id)) {
+            throw new NotFoundException();
+        }
+        repository.deleteById(id);
     }
 }
